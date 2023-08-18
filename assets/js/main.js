b103e92fd7bf8135d8106020c3d42aa1a472d1bc
OmarAliSoliman <omarasmar9955@gmail.com>
2023-08-18 09:54:16 +0300

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,29 @@ $(document).ready(function () {
     $(".custom_select").niceSelect();
   }
 
+
+  if($(".images_slider").length){
+    $(".small_images .thubm_img").on('click', function (e) {
+      let imgsrc = $(this).find("img").attr("src");
+      $(".images_slider .big_imag img").attr("src", imgsrc);
+    })
+  }
+
+
+  if($(".peaces_options").length){
+    $(".peaces_options .peaces_plus").on('click', function (e) {
+      let count = parseInt($(this).parent().find("input").val())
+      $(this).parent().find("input").val(count+1)
+    })
+
+    $(".peaces_options .peaces_minus").on('click', function (e) {
+      let count = parseInt($(this).parent().find("input").val())
+      if(count == 0){
+        count = 0
+      }else{
+        $(this).parent().find("input").val(count-1)
+      }
+    })
+  }
+
 });
